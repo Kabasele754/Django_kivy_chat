c89676b0f0a52c32466303c67c58466bc4a62b4d
Achille kabasele <pepexykabasele@gmail.com>
2022-08-26 20:57:32 +0200

    first commit

--- backend/chatApp/serializer.py
+++ backend/chatApp/serializer.py
@@ -1,5 +1,7 @@
 from unicodedata import name
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.validators import ValidationError
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from .models import *
 
@@ -22,11 +24,33 @@ class ProfilSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Profile
-        fields = ('user' ,'name','pic', 'friends') 
+        fields = ('user' ,'name','pic') 
+    
+    def validate(self,attrs):
+        email=Profile.objects.filter(username=attrs.get('username')).exists()
+
+        if email:
+            raise ValidationError(detail="User with email exists",code=status.HTTP_403_FORBIDDEN)
+
+        username=Profile.objects.filter(username=attrs.get('username')).exists()
+
+        if username:
+            raise ValidationError(detail="User with username exists",code=status.HTTP_403_FORBIDDEN)
+
+        return super().validate(attrs)
         
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'], validated_data['file'])
-        return user
+    # def create(self, validated_data):
+    #     user = Profile.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'], validated_data['file'])
+    #     return user
+    
+    def create(self,validated_data):
+        new_user=Profile(**validated_data)
+
+        new_user.password=make_password(validated_data.get('password'))
+
+        new_user.save()
+
+        return new_user
 
 
         
